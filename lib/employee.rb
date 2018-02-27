dbf6f83a982c1bcd4803b74a50d4483e1631662b
pollockcl <pollock.clee@gmail.com>
2018-02-27 09:40:47 -0700

    created employee class with tests

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,2 +1,10 @@
 class Employee
+  attr_reader :id, :name, :role, :start_date, :end_date
+  def initialize(attributes)
+    @id = attributes[:id].to_i
+    @name = attributes[:name]
+    @role = attributes[:role]
+    @start_date = Time.parse(attributes[:start_date])
+    @end_date   = Time.parse(attributes[:end_date])
+  end
 end
